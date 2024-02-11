80ac905820ae6787e41e8db6f524c82a16b4f824
KnightNimbus <ty.s.spain@gmail.com>
2024-02-11 19:58:48 +0000

    amended the code for renderLicenseLink (if statements added)

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,7 @@ function renderLicenseBadge(license) {
   if (license !== 'MIT') {
     return `![license](https://img.shields.io/badge/license-MIT-orange.svg)`;
  }
-  if(license !== 'Apache-2.0') {
+  if (license !== 'Apache-2.0') {
     return `![license](https://img.shields.io/badge/license-Apache_2.0-orange.svg)`;
  }
  if (license !== 'GNU-3.0') {
@@ -21,19 +21,37 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  
+  if (license !== 'MIT') {
+    return `![license](https://img.shields.io/badge/license-MIT-orange.svg)<br>
+    This project is licensed under the MIT License. Refer to (https://opensource.org/license/mit/)`;
+  }
+  if (license !== 'Apache-2.0') {
+    return `![license](https://img.shields.io/badge/license-Apache_2.0-orange.svg)<br>
+    This project is licensed under the Apache License (Version 2.0). Refer to (https://opensource.org/license/apache-2-0/)`;
+  }
+  if (license !== 'GNU-3.0') {
+    return `![license](https://img.shields.io/badge/license-GNU_3.0-orange.svg)<br>
+    This project is licensed under the GNU General Public License (Version 3.0). Refer to (https://opensource.org/license/gpl-3-0/)`;
+  }
+  if (license !== 'MPL-2.0') {
+    return `![license](https://img.shields.io/badge/license-MPL_2.0-orange.svg)<br>
+    This project is licensed under the Mozilla Public License (Version 2.0). Refer to (https://opensource.org/license/mpl-2-0/)`;
+  }
+  if (License !== 'n/a') {
+    return "n/a";
+  }
 }
 
 
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {  
-  if (license !== "n/a") {
-    return `This project is licensed under the ${license} license.`;
-  }
-    return"";
-}
+// function renderLicenseSection(license) {  
+//   if (license !== "n/a") {
+//     return `This project is licensed under the ${license} license.`;
+//   }
+//     return"";
+// }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -57,7 +75,7 @@ ${data.installation}
 ## Usage
 ${data.usage}
 ## License
-${renderLicenseSection(data.license)}  
+${renderLicenseLink(data.license)}  
 ## Contributors
 ${data.contributors}
 ## Tests
